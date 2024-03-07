bdfd06966d7b6976da93138bd35d75d84a1702f5
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-03-08 00:37:52 +0100

    Auto-commit: saved init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 --[[
+
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
 =====================================================================
@@ -245,7 +246,7 @@ vim.api.nvim_create_autocmd('QuitPre', {
 })
 
 vim.api.nvim_create_autocmd('QuitPre', {
-  pattern = '$MYVIMCONFIG/*.lua',
+  pattern = '*.lua',
   group = vim.api.nvim_create_augroup('jj-autocommit-nvim', { clear = true }),
   command = [[execute ':silent ! if git rev-parse --git-dir > /dev/null 2>&1 ; then git add % ; git commit -m "Auto-commit: saved %"; git push; fi > /dev/null 2>&1']],
 })
