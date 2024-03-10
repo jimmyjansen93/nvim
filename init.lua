315c9fde1bff635605b58c145cc439044d001d78
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-03-11 00:41:08 +0100

    Auto-commit: saved init.lua

--- init.lua
+++ init.lua
@@ -513,7 +513,7 @@ require('lazy').setup({
       }
     end,
     keys = {
-      { '<leader>cc', '<CMD>Telescope commander<CR>', mode = 'n', desc = 'Open commander' },
+      { '<leader>pc', '<CMD>Telescope commander<CR>', mode = 'n', desc = 'Open commander' },
     },
   },
 
@@ -728,15 +728,15 @@ require('lazy').setup({
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          map('<leader>cs', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
 
           -- Fuzzy find all the symbols in your current workspace
           --  Similar to document symbols, except searches over your whole project.
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          map('<leader>cw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
           -- Rename the variable under your cursor
           --  Most Language Servers support renaming across files, etc.
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('<leader>cr', vim.lsp.buf.rename, '[R]e[n]ame')
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
