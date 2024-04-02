3bef4419ec19544364e2729f2408177ef5772c4a
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-04-02 13:46:24 +0200

    Auto-commit: saved init.lua

--- init.lua
+++ init.lua
@@ -225,36 +225,34 @@ require('lazy').setup({
   {
     'anuvyklack/pretty-fold.nvim',
     opts = {
-      config = {
-        sections = {
-          left = {
-            'content',
-          },
-          right = {
-            ' ',
-            'number_of_folded_lines',
-            function(config)
-              return config.fill_char:rep(3)
-            end,
-          },
+      sections = {
+        left = {
+          'content',
         },
-        fill_char = '•',
-        remove_fold_markers = true,
-        keep_indentation = true,
-        process_comment_signs = 'spaces',
-        comment_signs = {},
-        stop_words = {
-          '@brief%s*', -- (for C++) Remove '@brief' and all spaces after.
+        right = {
+          ' ',
+          'number_of_folded_lines',
+          function(config)
+            return config.fill_char:rep(3)
+          end,
         },
-        add_close_pattern = true, -- true, 'last_line' or false
-        matchup_patterns = {
-          { '{', '}' },
-          { '%(', ')' }, -- % to escape lua pattern char
-          { '%[', ']' }, -- % to escape lua pattern char
-        },
-
-        ft_ignore = {},
       },
+      fill_char = '•',
+      remove_fold_markers = true,
+      keep_indentation = true,
+      process_comment_signs = 'spaces',
+      comment_signs = {},
+      stop_words = {
+        '@brief%s*', -- (for C++) Remove '@brief' and all spaces after.
+      },
+      add_close_pattern = true, -- true, 'last_line' or false
+      matchup_patterns = {
+        { '{', '}' },
+        { '%(', ')' }, -- % to escape lua pattern char
+        { '%[', ']' }, -- % to escape lua pattern char
+      },
+
+      ft_ignore = {},
     },
   },
 
