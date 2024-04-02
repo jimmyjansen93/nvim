9f84d89825e701bf4c02a0b7e537d3a5ef81bb90
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-04-02 03:10:04 +0200

    Auto-commit: saved init.lua

--- init.lua
+++ init.lua
@@ -251,7 +251,17 @@ require('lazy').setup({
     'tpope/vim-fugitive',
   },
 
-  { 'aserowy/tmux.nvim', opts = {} },
+  {
+    'aserowy/tmux.nvim',
+    opts = {
+      copy_sync = {
+        redirect_to_clipboard = true,
+      },
+      navigation = {
+        enable_default_keybindings = true,
+      },
+    },
+  },
 
   {
     'folke/trouble.nvim',
