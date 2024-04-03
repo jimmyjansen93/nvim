5f767aa23266ee0c3215ae62734e2daf239fb63a
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-04-03 16:36:40 +0200

    Auto-commit: saved init.lua

--- init.lua
+++ init.lua
@@ -307,37 +307,7 @@ require('lazy').setup({
 
   {
     'LukasPietzschmann/boo.nvim',
-    opts = {
-      -- win_opts will be used when creating the window. You can put everything here,
-      -- that vim.api.nvim_open_win (https://neovim.io/doc/user/api.html#nvim_open_win())
-      -- can handle.
-      win_opts = {
-        title = 'LSP Info',
-        title_pos = 'center',
-        relative = 'cursor',
-        row = 1,
-        col = 0,
-        style = 'minimal',
-        border = 'rounded',
-        focusable = true,
-      },
-      -- The window will not be wider than max_width (in character cells)
-      max_width = 80,
-      -- The window will not be taller than max_height (in character cells)
-      max_height = 20,
-      -- When the boo window is focused, pressing one of these will close it.
-      -- They will only be mapped in normalmode
-      escape_mappings = { 'q', '<esc>' },
-      -- Focus boo's window automatically after it's created
-      focus_on_open = true,
-      -- When the boo window is focused, and you'll focus another buffer,
-      -- the window will be closed when this is set to true
-      close_on_leave = true,
-      -- When moving the cursor in the buffer that boo was opened from, boo
-      -- will be closed. This makes most sense when paired with
-      -- `focus_on_open = false`
-      close_on_mouse_move = true,
-    },
+    opts = {},
     keys = {
       { '<leader>K', '<cmd>lua require("boo").boo()<cr>', desc = 'Lsp Info' },
     },
@@ -345,10 +315,10 @@ require('lazy').setup({
 
   {
     'IsWladi/Gittory',
-    opts = { -- you can omit this, is the default
-      atStartUp = true, -- If you want to initialize Gittory when Neovim starts
+    opts = {
+      atStartUp = true,
       notifySettings = {
-        enabled = false, -- This flag enables the notification system, allowing Gittory to send alerts about its operational status changes.
+        enabled = false,
       },
     },
   },
