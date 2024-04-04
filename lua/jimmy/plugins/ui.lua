7bcd5d4f5f8ca8b711a848206e79b2610a6763cd
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-04-05 00:10:28 +0200

    Auto-commit: saved /Users/jimmy/.config/nvim/lua/jimmy/plugins/ui.lua

--- lua/jimmy/plugins/ui.lua
+++ lua/jimmy/plugins/ui.lua
@@ -99,25 +99,14 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      local lualine = require 'lualine'
-      local lazy_status = require 'lazy.status' -- to configure lazy pending updates count
-
-      -- configure lualine with modified theme
-      lualine.setup {
+      require('lualine').setup {
         options = {
           theme = 'cattpuccin-mocha',
         },
         sections = {
-          lualine_x = {
-            {
-              lazy_status.updates,
-              cond = lazy_status.has_updates,
-              color = { fg = '#ff9e64' },
-            },
-            { 'encoding' },
-            { 'fileformat' },
-            { 'filetype' },
-          },
+          lualine_x = {},
+          lualine_y = { { 'filetype' } },
+          lualine_z = { { 'buffers' } },
         },
       }
     end,
