c3a761a15d1dea0437e90b0ce9ea86e9f1ae2a07
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-04-04 22:58:15 +0200

    Auto-commit: saved lua/jimmy/plugins/telescope.lua

--- lua/jimmy/plugins/telescope.lua
+++ lua/jimmy/plugins/telescope.lua
@@ -28,6 +28,7 @@ return {
             theme = 'ivy',
             hidden = { file_browser = true, folder_browser = true },
             hijack_netrw = true,
+            layout_config = { height = 0.5 },
           },
         },
       }
@@ -38,6 +39,7 @@ return {
       pcall(require('telescope').load_extension, 'conflicts')
 
       local builtin = require 'telescope.builtin'
+
       vim.keymap.set('n', '<leader>sc', '<CMD>Telescope conflicts<CR>', { desc = 'Search [C]onflicts' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Search Help' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'Search Keymaps' })
@@ -50,12 +52,8 @@ return {
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = 'Search Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>,', builtin.buffers, { desc = ', Find existing buffers' })
       vim.keymap.set('n', '<leader> ', builtin.find_files, { desc = 'Find File' })
-      vim.keymap.set(
-        'n',
-        '<leader>fb',
-        "<cmd>lua require('telescope').extensions.file_browser.file_browser { layout_config = { height = 0.5 } }<cr>",
-        { desc = '[F]ile [B]rowser' }
-      )
+      vim.keymap.set('n', '<leader>fr', '<CMD>Telescope file_browser<CR>', { desc = 'File Browser Root' })
+      vim.keymap.set('n', '<leader>fb', '<CMD>Telescope file_browser path=%:p:h<CR>', { desc = 'File Browser Current' })
       vim.keymap.set('n', '<leader>s\\', function()
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_ivy {
           previewer = false,
