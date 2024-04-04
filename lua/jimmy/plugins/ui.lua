815d3acf0f0fee30e39d4966fc17d20c8c4465ca
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-04-04 23:42:43 +0200

    Auto-commit: saved lua/jimmy/plugins/ui.lua

--- lua/jimmy/plugins/ui.lua
+++ lua/jimmy/plugins/ui.lua
@@ -95,105 +95,76 @@ return {
       },
     },
   },
-  -- {
-  --   'nvim-lualine/lualine.nvim',
-  --   event = 'VeryLazy',
-  --   init = function()
-  --     vim.g.lualine_laststatus = vim.o.laststatus
-  --     if vim.fn.argc(-1) > 0 then
-  --       -- set an empty statusline till lualine loads
-  --       vim.o.statusline = ' '
-  --     else
-  --       -- hide the statusline on the starter page
-  --       vim.o.laststatus = 0
-  --     end
-  --   end,
-  --   opts = function()
-  --     -- PERF: we don't need this lualine require madness 🤷
-  --     local lualine_require = require 'lualine_require'
-  --     lualine_require.require = require
-  --
-  --     vim.o.laststatus = vim.g.lualine_laststatus
-  --
-  --     return {
-  --       options = {
-  --         theme = 'auto',
-  --         globalstatus = true,
-  --         disabled_filetypes = { statusline = { 'dashboard', 'alpha', 'starter' } },
-  --       },
-  --       sections = {
-  --         lualine_a = { 'mode' },
-  --         lualine_b = { 'branch' },
-  --
-  --         lualine_c = {
-  --           LazyVim.lualine.root_dir(),
-  --           {
-  --             'diagnostics',
-  --             symbols = {
-  --               error = icons.diagnostics.Error,
-  --               warn = icons.diagnostics.Warn,
-  --               info = icons.diagnostics.Info,
-  --               hint = icons.diagnostics.Hint,
-  --             },
-  --           },
-  --           { 'filetype', icon_only = true, separator = '', padding = { left = 1, right = 0 } },
-  --           { LazyVim.lualine.pretty_path() },
-  --         },
-  --         lualine_x = {
-  --         -- stylua: ignore
-  --         {
-  --           function() return require("noice").api.status.command.get() end,
-  --           cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-  --           color = LazyVim.ui.fg("Statement"),
-  --         },
-  --         -- stylua: ignore
-  --         {
-  --           function() return require("noice").api.status.mode.get() end,
-  --           cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-  --           color = LazyVim.ui.fg("Constant"),
-  --         },
-  --         -- stylua: ignore
-  --         {
-  --           function() return "  " .. require("dap").status() end,
-  --           cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
-  --           color = LazyVim.ui.fg("Debug"),
-  --         },
-  --           {
-  --             require('lazy.status').updates,
-  --             cond = require('lazy.status').has_updates,
-  --             color = LazyVim.ui.fg 'Special',
-  --           },
-  --           {
-  --             'diff',
-  --             symbols = {
-  --               added = icons.git.added,
-  --               modified = icons.git.modified,
-  --               removed = icons.git.removed,
-  --             },
-  --             source = function()
-  --               local gitsigns = vim.b.gitsigns_status_dict
-  --               if gitsigns then
-  --                 return {
-  --                   added = gitsigns.added,
-  --                   modified = gitsigns.changed,
-  --                   removed = gitsigns.removed,
-  --                 }
-  --               end
-  --             end,
-  --           },
-  --         },
-  --         lualine_y = {
-  --           { 'progress', separator = ' ', padding = { left = 1, right = 0 } },
-  --           { 'location', padding = { left = 0, right = 1 } },
-  --         },
-  --         lualine_z = {
-  --           function()
-  --             return ' ' .. os.date '%R'
-  --           end,
-  --         },
-  --       },
-  --       extensions = { 'neo-tree', 'lazy' },
-  --     }
-  --   end,
-  -- },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local lualine = require 'lualine'
+      local lazy_status = require 'lazy.status' -- to configure lazy pending updates count
+
+      local colors = {
+        blue = '#65D1FF',
+        green = '#3EFFDC',
+        violet = '#FF61EF',
+        yellow = '#FFDA7B',
+        red = '#FF4A4A',
+        fg = '#c3ccdc',
+        bg = '#112638',
+        inactive_bg = '#2c3043',
+      }
+
+      local my_lualine_theme = {
+        normal = {
+          a = { bg = colors.blue, fg = colors.bg, gui = 'bold' },
+          b = { bg = colors.bg, fg = colors.fg },
+          c = { bg = colors.bg, fg = colors.fg },
+        },
+        insert = {
+          a = { bg = colors.green, fg = colors.bg, gui = 'bold' },
+          b = { bg = colors.bg, fg = colors.fg },
+          c = { bg = colors.bg, fg = colors.fg },
+        },
+        visual = {
+          a = { bg = colors.violet, fg = colors.bg, gui = 'bold' },
+          b = { bg = colors.bg, fg = colors.fg },
+          c = { bg = colors.bg, fg = colors.fg },
+        },
+        command = {
+          a = { bg = colors.yellow, fg = colors.bg, gui = 'bold' },
+          b = { bg = colors.bg, fg = colors.fg },
+          c = { bg = colors.bg, fg = colors.fg },
+        },
+        replace = {
+          a = { bg = colors.red, fg = colors.bg, gui = 'bold' },
+          b = { bg = colors.bg, fg = colors.fg },
+          c = { bg = colors.bg, fg = colors.fg },
+        },
+        inactive = {
+          a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = 'bold' },
+          b = { bg = colors.inactive_bg, fg = colors.semilightgray },
+          c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+        },
+      }
+
+      -- configure lualine with modified theme
+      lualine.setup {
+        options = {
+          theme = my_lualine_theme,
+        },
+        sections = {
+          lualine_x = {
+            {
+              lazy_status.updates,
+              cond = lazy_status.has_updates,
+              color = { fg = '#ff9e64' },
+            },
+            { 'encoding' },
+            { 'fileformat' },
+            { 'filetype' },
+          },
+        },
+      }
+    end,
+  },
 }
