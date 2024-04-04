efb6fb13d9c6cb9ca1c94ba2dda3520ba945e965
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-04-05 00:28:13 +0200

    Auto-commit: saved init.lua

--- init.lua
+++ init.lua
@@ -48,8 +48,10 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
 vim.keymap.set('n', '<S-h>', '<CMD>bnext<cr>', { desc = 'Next buffer' })
 vim.keymap.set('n', '<S-l>', '<CMD>bprev<cr>', { desc = 'Previous buffer' })
+vim.keymap.set('n', '<S-q>', '<CMD>bd<cr>', { desc = 'Kill buffer' })
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
