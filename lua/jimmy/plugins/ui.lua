92325c1d30c1b3ca89ad5024e19217d19cf8733c
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-04-05 00:16:38 +0200

    Auto-commit: saved /Users/jimmy/.config/nvim/lua/jimmy/plugins/ui.lua

--- lua/jimmy/plugins/ui.lua
+++ lua/jimmy/plugins/ui.lua
@@ -105,6 +105,14 @@ return {
           lualine_y = { { 'buffers' } },
           lualine_z = {},
         },
+        inactive_sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = {},
+          lualine_y = { { 'buffers' } },
+          lualine_z = {},
+        },
       }
     end,
   },
