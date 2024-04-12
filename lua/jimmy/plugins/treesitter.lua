11ea891b4c0cbffdffb65827f1b88857ca9b6f2c
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-04-12 13:58:22 +0200

    feat: add treesitter textobjects

--- lua/jimmy/plugins/treesitter.lua
+++ lua/jimmy/plugins/treesitter.lua
@@ -50,4 +50,20 @@ return {
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        textobjects = {
+          select = {
+            enable = true,
+          },
+        },
+      }
+    end,
+  },
 }
