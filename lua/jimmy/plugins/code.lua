b6181d177231da9add9dc1e7ffb77eee03fcdf42
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-04-17 00:20:23 +0200

    feat: slowly move away from `keys` and use vim.keymap.set

--- lua/jimmy/plugins/code.lua
+++ lua/jimmy/plugins/code.lua
@@ -1,44 +1,30 @@
 return {
   { 'numToStr/Comment.nvim', lazy = false, opts = {} },
+  { 'gpanders/editorconfig.nvim' },
 
   {
     'folke/trouble.nvim',
     cmd = { 'TroubleToggle', 'Trouble' },
-    opts = { use_diagnostic_signs = true },
-    keys = {
-      { '<leader>xx', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Document Diagnostics' },
-      { '<leader>xX', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Workspace Diagnostics' },
-      { '<leader>xL', '<cmd>TroubleToggle loclist<cr>', desc = 'Location List' },
-      { '<leader>xQ', '<cmd>TroubleToggle quickfix<cr>', desc = 'Quickfix List' },
-      {
+    config = function()
+      require('trouble').setup { use_diagnostic_signs = true }
+
+      vim.keymap.set('n', '<leader>xx', '<cmd>TroubleToggle document_diagnostics<cr>', { noremap = true, silent = true, desc = 'Document Diagnostics' })
+      vim.keymap.set('n', '<leader>xX', '<cmd>TroubleToggle workspace_diagnostics<cr>', { noremap = true, silent = true, desc = 'Workspace Diagnostics' })
+      vim.keymap.set('n', '<leader>xL', '<cmd>TroubleToggle loclist<cr>', { noremap = true, silent = true, desc = 'Location List' })
+      vim.keymap.set('n', '<leader>xQ', '<cmd>TroubleToggle quickfix<cr>', { noremap = true, silent = true, desc = 'Quickfix List' })
+      vim.keymap.set(
+        'n',
         '[q',
-        function()
-          if require('trouble').is_open() then
-            require('trouble').previous { skip_groups = true, jump = true }
-          else
-            local ok, err = pcall(vim.cmd.cprev)
-            if not ok then
-              vim.notify(err, vim.log.levels.ERROR)
-            end
-          end
-        end,
-        desc = 'Previous trouble/quickfix item',
-      },
-      {
+        '<cmd>lua require("trouble").previous { skip_groups = true, jump = true }<cr>',
+        { noremap = true, silent = true, desc = 'Previous trouble/quickfix item' }
+      )
+      vim.keymap.set(
+        'n',
         ']q',
-        function()
-          if require('trouble').is_open() then
-            require('trouble').next { skip_groups = true, jump = true }
-          else
-            local ok, err = pcall(vim.cmd.cnext)
-            if not ok then
-              vim.notify(err, vim.log.levels.ERROR)
-            end
-          end
-        end,
-        desc = 'Next trouble/quickfix item',
-      },
-    },
+        '<cmd>lua require("trouble").next { skip_groups = true, jump = true }<cr>',
+        { noremap = true, silent = true, desc = 'Next trouble/quickfix item' }
+      )
+    end,
   },
 
   {
@@ -67,10 +53,6 @@ return {
       { '<leader>xs', '<cmd>TodoTelescope<cr>', desc = 'Todo' },
       { '<leader>xS', '<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>', desc = 'Todo/Fix/Fixme' },
     },
-  },
-
-  {
-    'gpanders/editorconfig.nvim',
   },
 
   {
