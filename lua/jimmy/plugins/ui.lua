1006616dcf08744adbe76435a54500cafb40328b
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-04-20 00:25:40 +0200

    feat: disable statusline for filetypes

--- lua/jimmy/plugins/ui.lua
+++ lua/jimmy/plugins/ui.lua
@@ -5,17 +5,23 @@ return {
     config = function()
       require('lualine').setup {
         sections = {
-          lualine_x = {},
-          lualine_y = { { 'buffers' } },
-          lualine_z = {},
-        },
-        inactive_sections = {
           lualine_a = { 'mode' },
           lualine_b = { 'branch', 'diff', 'diagnostics' },
           lualine_c = { 'filename' },
           lualine_x = {},
           lualine_y = { { 'buffers' } },
           lualine_z = {},
+        },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {},
+        },
+        disabled_filetypes = {
+          statusline = { 'neogit', 'overseer' },
         },
       }
     end,
