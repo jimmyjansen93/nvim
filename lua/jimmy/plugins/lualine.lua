d81c1e8e86dce6e270194e3f17f710a47e7a0e85
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-08-14 21:58:31 +0200

    fix: resolve issues with macro recording

--- lua/jimmy/plugins/lualine.lua
+++ lua/jimmy/plugins/lualine.lua
@@ -29,7 +29,13 @@ return {
           lualine_c = {
             'filename',
           },
-          lualine_x = {},
+          lualine_x = {
+            {
+              require('noice').api.statusline.mode.get,
+              cond = require('noice').api.statusline.mode.has,
+              color = { fg = '#ff9e64' },
+            },
+          },
           lualine_y = { 'filetype' },
           lualine_z = { 'diff', 'branch' },
         },
