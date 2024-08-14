8487ab6b9e44ac35fe464ee9061e17bb7cfa95de
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-08-15 00:29:03 +0200

    feat: add completion keybind to cmp

--- lua/jimmy/plugins/cmp.lua
+++ lua/jimmy/plugins/cmp.lua
@@ -3,32 +3,9 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      {
-        'zbirenbaum/copilot-cmp',
-        config = function()
-          require('copilot_cmp').setup()
-        end,
-      },
-      -- {
-      --   'zbirenbaum/copilot.lua',
-      --   opts = {
-      --     panel = { enabled = false },
-      --     suggestion = { enabled = false },
-      --   },
-      -- },
-
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
-      -- 'hrsh7th/cmp-nvim-lsp-signature-help',
-      {
-        'David-Kunz/cmp-npm',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        ft = 'json',
-        config = function()
-          require('cmp-npm').setup {}
-        end,
-      },
-
+      'hrsh7th/cmp-nvim-lsp-signature-help',
       'onsails/lspkind.nvim',
     },
     config = function()
@@ -43,12 +20,12 @@ return {
               return math.floor(0.4 * vim.o.columns)
             end,
             ellipsis_char = '...',
-            symbol_map = { Copilot = '' },
           },
         },
         completion = { completeopt = 'menu,menuone,noinsert' },
 
         mapping = cmp.mapping.preset.insert {
+          ['<C-x>'] = cmp.mapping.complete(),
           ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
           ['<C-y>'] = cmp.mapping.confirm { select = true },
@@ -57,7 +34,6 @@ return {
           { name = 'nvim_lsp' },
           { name = 'nvim_lsp_signature_help' },
           { name = 'path' },
-          { name = 'npm', keyword_length = 4 },
         },
       }
     end,
