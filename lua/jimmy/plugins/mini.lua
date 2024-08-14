5e779a5c849e8cffe2d7814efa8222e4835037bf
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-08-14 17:04:08 +0200

    feat: enable mini.icons

--- lua/jimmy/plugins/mini.lua
+++ lua/jimmy/plugins/mini.lua
@@ -1,3 +1,11 @@
 return {
-  { 'echasnovski/mini.nvim', version = false },
+  {
+    'echasnovski/mini.nvim',
+    version = false,
+    config = function()
+      require('mini.icons').setup {
+        style = 'glyph',
+      }
+    end,
+  },
 }
