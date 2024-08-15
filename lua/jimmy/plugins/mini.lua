e37aaff664a188d95f7aa693c12747584694d075
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-08-16 00:13:55 +0200

    feat: add mini.move

--- lua/jimmy/plugins/mini.lua
+++ lua/jimmy/plugins/mini.lua
@@ -37,6 +37,29 @@ return {
         use_icons = true,
         set_vim_settings = true,
       }
+
+      require('mini.move').setup {
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+          -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+          left = '<M-h>',
+          right = '<M-l>',
+          down = '<M-j>',
+          up = '<M-k>',
+
+          -- Move current line in Normal mode
+          line_left = '<M-h>',
+          line_right = '<M-l>',
+          line_down = '<M-j>',
+          line_up = '<M-k>',
+        },
+
+        -- Options which control moving behavior
+        options = {
+          -- Automatically reindent selection during linewise vertical move
+          reindent_linewise = true,
+        },
+      }
     end,
   },
 }
