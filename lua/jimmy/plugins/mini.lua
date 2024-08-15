749ac762f3f77951ffa27dabd85533c3e6f86b75
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-08-15 05:55:17 +0200

    feat: use mini.statusline instead of lualine

--- lua/jimmy/plugins/mini.lua
+++ lua/jimmy/plugins/mini.lua
@@ -6,6 +6,37 @@ return {
       require('mini.icons').setup {
         style = 'glyph',
       }
+      require('mini.git').setup()
+      require('mini.diff').setup()
+
+      require('mini.statusline').setup {
+        content = {
+          active = function()
+            local MiniStatusline = require 'mini.statusline'
+
+            local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+            local git = MiniStatusline.section_git { trunc_width = 40 }
+            local diff = MiniStatusline.section_diff { trunc_width = 75 }
+            local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+            local lsp = MiniStatusline.section_lsp { trunc_width = 75 }
+            local filename = MiniStatusline.section_filename { trunc_width = 120 }
+
+            return MiniStatusline.combine_groups {
+              { hl = mode_hl, strings = { mode } },
+              { hl = 'MiniStatuslineDevinfo', strings = { diagnostics, lsp } },
+              '%<', -- Mark general truncate point
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
+              '%=', -- End left alignment
+              { hl = 'MiniStatuslineDevinfo', strings = { diff } },
+              { hl = 'MiniStatuslineDevinfo', strings = { git } },
+            }
+          end,
+          inactive = nil,
+        },
+
+        use_icons = true,
+        set_vim_settings = true,
+      }
     end,
   },
 }
