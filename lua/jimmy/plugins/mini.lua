52cd2761cd3ddde4ef7eed65d0eee532bfeaa766
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-08-23 15:39:00 +0200

    feat: adjust mini to use surround

--- lua/jimmy/plugins/mini.lua
+++ lua/jimmy/plugins/mini.lua
@@ -8,7 +8,32 @@ return {
       }
       require('mini.git').setup()
       require('mini.diff').setup()
-      -- TODO: add surround with proper keybinds
+
+      require('mini.surround').setup {
+        -- :h MiniSurround.config
+        custom_surroundings = nil,
+
+        -- :h MiniSurround.config
+        search_method = 'cover',
+
+        highlight_duration = 500,
+        n_lines = 20,
+        respect_selection_type = false,
+        silent = false,
+
+        mappings = {
+          add = 'sa', -- Add surrounding in Normal and Visual modes
+          delete = 'sd', -- Delete surrounding
+          find = 'sf', -- Find surrounding (to the right)
+          find_left = 'sF', -- Find surrounding (to the left)
+          highlight = 'sh', -- Highlight surrounding
+          replace = 'sr', -- Replace surrounding
+          update_n_lines = 'sn', -- Update `n_lines`
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      }
 
       require('mini.statusline').setup {
         content = {
