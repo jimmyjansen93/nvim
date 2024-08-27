6e2973eb1c564f4041cd6b4029df6ec728c73ead
Jimmy Jansen <jimmyjansen93@gmail.com>
2024-08-27 18:01:11 +0200

    feat: remove hardtime as it is annoying

--- lua/jimmy/plugins/hardtime.lua
+++
@@ -1,8 +0,0 @@
--- TODO: still in doubt whether to keep this here, can probably remove
-return {
-  {
-    'm4xshen/hardtime.nvim',
-    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
-    opts = {},
-  },
-}
