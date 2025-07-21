fe35b2065c11c1a6aad15596fc3f9bcaa3b1e6e8
Jimmy Jansen <jimmyjansen@storyteq.com>
2025-07-21 20:57:35 +0200

    chore: auto-update config

--- lua/jimmy/plugins/git.lua
+++ lua/jimmy/plugins/git.lua
@@ -1,35 +1,80 @@
 return {
   {
-    'NeogitOrg/neogit',
+    "NeogitOrg/neogit",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'sindrets/diffview.nvim',
-      'nvim-telescope/telescope.nvim',
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
     },
     config = function()
-      require('neogit').setup {
+      require("neogit").setup({
         disable_signs = false,
         use_per_project_settings = true,
         remember_settings = true,
-        graph_style = 'unicode',
+        graph_style = "unicode",
         ignored_settings = {
-          'NeogitPushPopup--force',
-          'NeogitPullPopup--rebase',
-          'NeogitCommitPopup--allow-empty',
-          'NeogitRevertPopup--no-edit',
+          "NeogitPushPopup--force",
+          "NeogitPullPopup--rebase",
+          "NeogitCommitPopup--allow-empty",
+          "NeogitRevertPopup--no-edit",
         },
-      }
+      })
 
-      vim.keymap.set('n', '<leader>gg', '<CMD>Neogit<CR>', { desc = 'Open git window' })
+      vim.keymap.set("n", "<leader>gg", "<CMD>Neogit<CR>", { desc = "Open git window" })
     end,
   },
   {
-    'IsWladi/Gittory',
+    "IsWladi/Gittory",
     opts = {
       atStartUp = true,
       notifySettings = {
         enabled = false,
       },
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
+      },
+      signs_staged = {
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
+      },
+      signs_staged_enable = true,
+      signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+      numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      watch_gitdir = {
+        follow_files = true,
+      },
+      auto_attach = true,
+      attach_to_untracked = false,
+      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+      current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+      sign_priority = 6,
+    },
+    keys = {
+      { "<leader>gb", "<CMD>Gitsigns toggle_current_line_blame<CR>", "Toggle blame" },
     },
   },
 }
